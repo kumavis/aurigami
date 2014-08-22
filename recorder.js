b70939230a65eaf593180efd83349cfe03b8eae5
aaron <a.aa.akio.aaron@gmail.com>
2014-08-21 21:12:49 -0700

    Initial Commit

---
+++ recorder.js
@@ -0,0 +1,58 @@
+
+var client = new BinaryClient('ws://localhost:9001');
+
+client.on('open', function() {
+
+  var connection = null
+
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia({audio:true}, success, function(e) {
+      alert('Error capturing audio.');
+    });
+  } else alert('getUserMedia not supported in this browser.');
+
+  var recording = false;
+
+  window.startRecording = function() {
+    connection = client.createStream({this:"That", foo:"Bar"});
+    recording = true;
+  }
+
+  window.stopRecording = function() {
+    recording = false
+    connection.end();
+  }
+
+  function success(e) {
+    audioContext = window.AudioContext || window.webkitAudioContext;
+    context = new audioContext();
+
+    // the sample rate is in context.sampleRate
+    audioInput = context.createMediaStreamSource(e);
+
+    var bufferSize = 2048;
+    recorder = context.createScriptProcessor(bufferSize, 1, 1);
+
+    recorder.onaudioprocess = function(e){
+      if(!recording) return;
+      console.log ('recording');
+      var left = e.inputBuffer.getChannelData(0);
+      connection.write(convertoFloat32ToInt16(left));
+    }
+
+    audioInput.connect(recorder)
+    recorder.connect(context.destination);
+  }
+
+  function convertoFloat32ToInt16(buffer) {
+    var l = buffer.length;
+    var buf = new Int16Array(l)
+
+    while (l--) {
+      buf[l] = buffer[l]*0xFFFF;    //convert to 16 bit
+    }
+    return buf.buffer
+  }
+});
